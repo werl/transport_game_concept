40e4cb40ca59e5ce39c4952c8bf3b79e254445b1
Peter Lewis <peter@werl.me>
2017-11-01 19:18:01 -0500

    allow Tiles to be created without having one in the scene by default

--- Assets/Scripts/WorldCreator.cs
+++ Assets/Scripts/WorldCreator.cs
@@ -7,21 +7,22 @@ public class WorldCreator : MonoBehaviour {
 	public int width = 10;
 	public int length = 10;
 
-	public Tile tile;
+	public GameObject tile;
 
 	protected Dictionary<TileCoordinate, Tile> Tiles = new Dictionary<TileCoordinate, Tile>();
 
 	void Start () {
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < length; j++) {
-				Tile t = GameObject.Instantiate (tile) as Tile;
-				t.name = "Tile " + i + ", " + j;
-				t.transform.parent = transform;
-				t.transform.SetPositionAndRotation (new Vector3 (i * 3, 0, j * 3), Quaternion.identity);
+				GameObject o = GameObject.Instantiate (tile);
+				o.name = "Tile " + i + ", " + j;
+				o.transform.parent = transform;
+				o.transform.SetPositionAndRotation (new Vector3 (i * 3, 0, j * 3), Quaternion.identity);
 
-				Tiles.Add (new TileCoordinate (i, j), t);
+				Tiles.Add (new TileCoordinate (i, j), o.GetComponent<Tile>());
 			}
 		}
+		tile.gameObject.SetActive (false);
 	}
 	
 	void Update () {
